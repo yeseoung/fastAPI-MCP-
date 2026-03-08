4f9fa195903d90cb904a77b8109c2da3490d94b1
VOLVOVSKY <90137909+yeseoung@users.noreply.github.com>
2026-03-08 22:17:22 +0900

    Update Student_repo.py

--- repositories/Student_repo.py
+++ repositories/Student_repo.py
@@ -1,10 +1,6 @@
 from db.DB_conn import get_stu_conn
 from pymysql.cursors import DictCursor
 
-#역할: 데이터베이스와 직접 통신
-#특징: SQL 실행, 결과 반환, DB 연결 관리
-#주의: API 계층(FastAPI 라우터)에서는 반환값을 받아서 직접 응답 처리만 하면 됨
-
 def get_students():
     conn = get_stu_conn()
     try:
@@ -36,13 +32,12 @@ def get_student_by_name(student_name : str):
         conn.close()
 
 def get_random_students(limit=5):
-    conn = get_stu_conn()  # 기존에 만드신 DB 연결 함수
+    conn = get_stu_conn()
     try:
         with conn.cursor() as cursor:
-            # SQL의 RAND() 함수를 사용해 행을 섞고 지정한 수만큼 가져옵니다.
             sql = "SELECT name, student_id FROM stu ORDER BY RAND() LIMIT %s"
             cursor.execute(sql, (limit,))
             result = cursor.fetchall()
-            return result # 리스트 형태 [{name: '...', student_id: '...'}, ...]
+            return result
     finally:
         conn.close()
